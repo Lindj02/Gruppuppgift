e5a4462a9dd8b478e3048049106a0df551ef6061
André Lindqvist <71825454+lindqvistandre@users.noreply.github.com>
2023-10-04 14:40:09 +0200

    Lagt till user.cs

---
+++ User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gruppuppgift
+{
+        public class User
+        {
+            private const string Username = "admin";
+            private const string Password = "psw12345";
+
+            public bool Authenticate(string inputUsername, string inputPassword)
+            {
+                return inputUsername == Username && inputPassword == Password;
+            }
+        }
+    }
+}
