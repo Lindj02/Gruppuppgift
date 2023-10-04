a7852a09807c22aa6d4d85650e2551dd23812799
Michel Shabankareh <michel.shaban99@gmail.com>
2023-10-04 14:34:43 +0200

    Lagt till recept klass

---
+++ Recipes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gruppuppgift
+{
+    internal class Recipes
+    {
+        public class Recept
+        {
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public string PicturePatch { get; set; } // Länk till bilder
+            public string Type { get; set; } // Typer av recept
+        }
+    }
+}
