6e5aa06699d644dc37dc7171c24a6daae094e68c
jonatlsun <jorgetlpereda@gmail.com>
2023-10-29 02:00:52 +0200

    errorLogs - Form

---
+++ ErrorForm.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Gruppuppgift
+{
+    partial class ErrorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ErrorForm));
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.BackgroundColor = Color.Indigo;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(-2, 0);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(557, 165);
+            dataGridView1.TabIndex = 0;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // ErrorForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Indigo;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(551, 262);
+            Controls.Add(dataGridView1);
+            Name = "ErrorForm";
+            Text = "ErrorForm";
+            Load += ErrorForm_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+    }
+}
---
+++ Logger.cs
@@ -0,0 +1,81 @@
+﻿///////////////////////////////////////////////////////////////////////////////
+//////////////////////////// Jorge Jonathan Pereda ////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.IO;
+
+namespace Gruppuppgift
+{
+    public class Logger
+    {
+        private List<LogEntry> logEntries;
+
+        public event EventHandler<LogAddedEventArgs> LogAdded;
+
+        public Logger()
+        {
+            logEntries = new List<LogEntry>();
+        }
+
+        public class LogEntry
+        {
+
+            public DateTime Timestamp { get; set; }
+            public string Message { get; set; }
+
+        }
+
+        public class LogAddedEventArgs : EventArgs
+        {
+
+            public string LogMessage { get; set; }
+
+        }
+
+
+        public void AddLogEntry(string message)
+        {
+            logEntries.Add(new LogEntry
+            {
+
+                Timestamp = DateTime.Now,
+                Message = message
+
+            });
+
+            // Trigger the event
+            LogAdded?.Invoke(this, new LogAddedEventArgs { LogMessage = message });
+
+        }
+
+        public void SaveLogsToFile(string filePath)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    foreach (var entry in logEntries)
+                    {
+                        string formattedTimestamp = entry.Timestamp.ToString("yyyy-MM-dd HH:mm");
+                        writer.WriteLine($"{{ \"timestamp\": \"{formattedTimestamp}\", \"message\": \"{entry.Message}\" }}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error writing log file: {ex.Message}");
+            }
+        }
+
+        public List<LogEntry> GetLogEntries()
+        {
+            return logEntries;
+        }
+
+
+        
+    }
+}
